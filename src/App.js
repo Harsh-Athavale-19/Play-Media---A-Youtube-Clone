4a45ff4d225e7d23860e4db8eeadacacd71d37ad
HARSH ASHOK ATHAVALE <harshathavale190920@gmail.com>
2023-03-17 16:22:04 +0530

    Completed Navbar Design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import { Box } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-
-    </div>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
