1691dd2ac986a6e8e843b4d36b51e91b21dd9e11
HARSH ASHOK ATHAVALE <harshathavale190920@gmail.com>
2023-03-17 21:56:59 +0530

    Successfully Fetched Data from API

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,9 +6,12 @@ import { fetchFromAPI } from "./../utils/fetchFromAPI";
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
   }, [selectedCategory]);
 
   return (
@@ -43,7 +46,7 @@ const Feed = () => {
         >
           {selectedCategory} <span style={{ color: "#F31503" }}>Videos</span>
         </Typography>
-        <Videos videos={[]} />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
